71917b398a3d51cb5f42521acf216c153fa99b96
gitjonaspeeters <71467643+gitjonaspeeters@users.noreply.github.com>
2022-11-26 15:34:47 +0100

    l

--- src/main/java/com/pokemoncoin/rest/RestApplication.java
+++ src/main/java/com/pokemoncoin/rest/RestApplication.java
@@ -2,8 +2,10 @@ package com.pokemoncoin.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RestApplication {
 
 	public static void main(String[] args) {
