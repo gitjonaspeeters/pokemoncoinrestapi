e3ca038a7de672638d0b15f96a59591c4093808b
gitjonaspeeters <71467643+gitjonaspeeters@users.noreply.github.com>
2022-11-26 15:26:46 +0100

    l

--- src/main/java/com/pokemoncoin/rest/Repo/Repo.java
+++ src/main/java/com/pokemoncoin/rest/Repo/Repo.java
@@ -2,7 +2,9 @@ package com.pokemoncoin.rest.Repo;
 
 import com.pokemoncoin.rest.Model.pokemoncoin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface Repo extends JpaRepository<pokemoncoin, Long> {
 
 }
